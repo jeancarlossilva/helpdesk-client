5eb0b11f5a680dc8b2a85f31ada9fb8225d6090e
jeancarlossilvacc <jeancarlossilvacc@gmail.com>
2022-08-27 10:34:45 -0300

    Component Home criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,11 +28,13 @@ import { MatCardModule } from '@angular/material/card';
 
 //componentes de projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
